95d7564c659b525c47d130d50071cd63db7f5412
jaber.abbas <jaber.h.abbas@gmail.com>
2023-02-17 16:37:39 +0100

    update

--- src/test/java/com/entity/service/EmployeeServiceTest.java
+++ src/test/java/com/entity/service/EmployeeServiceTest.java
@@ -1,7 +1,8 @@
 package com.entity.service;
 
-import com.entity.dao.DepartmentRepository;
+import com.entity.dao.EmployeeRepository;
 import com.entity.model.Department;
+import com.entity.model.Employee;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
@@ -24,30 +25,35 @@ import static org.mockito.Mockito.when;
 public class EmployeeServiceTest {
 
     @InjectMocks
-    private DepartmentService departmentService;
+    private EmployeeService employeeService;
 
     @Mock
-    DepartmentRepository departmentRepository;
+    EmployeeRepository employeeRepository;
 
 
     @Test
     public void testCreate() {
         Department department = new Department(Long.valueOf(1), "dept", "department service unit test", "Paris", null);
-        when(departmentRepository.save(department)).thenReturn(department);
-        Department savedDept = departmentService.create(department);
-        assertThat(savedDept.getName()).isEqualTo("dept");
-        assertThat(savedDept.getLocation()).isEqualTo("Paris");
+        Employee employee = new Employee(Long.valueOf(1), "Bart", "Vanden", Double.valueOf(2000), 35, "Architect", department);
+        when(employeeRepository.save(employee)).thenReturn(employee);
+        Employee savedEmpl = employeeService.create(employee);
+        assertThat(savedEmpl.getFirstName()).isEqualTo("Bart");
+        assertThat(savedEmpl.getLastName()).isEqualTo("Vanden");
+        assertThat(savedEmpl.getDepartment().getId()).isEqualTo(1);
+        assertThat(savedEmpl.getDepartment().getLocation()).isEqualTo(department.getLocation());
     }
 
     @Test
     public void testGet() {
         Department department = new Department(Long.valueOf(1), "dept", "department service unit test", "Paris", null);
-        when(departmentRepository.findById(anyLong())).thenReturn(Optional.of(department));
-        Optional<Department> optionalDepartment = departmentService.findById(Long.valueOf(1));
-        if (optionalDepartment.isPresent()) {
-            Department department1 = optionalDepartment.get();
-            assertThat(department1.getName()).isEqualTo(department.getName());
-            assertThat(department1.getLocation()).isEqualTo(department.getLocation());
+        Employee employee = new Employee(Long.valueOf(1), "Bart", "Vanden", Double.valueOf(2000), 35, "Architect", department);
+        when(employeeRepository.findById(anyLong())).thenReturn(Optional.of(employee));
+        Optional<Employee> optionalEmpl = employeeService.findById(Long.valueOf(1));
+        if (optionalEmpl.isPresent()) {
+            Employee employee1 = optionalEmpl.get();
+            assertThat(employee1.getFirstName()).isEqualTo(employee.getFirstName());
+            assertThat(employee1.getLastName()).isEqualTo(employee.getLastName());
+            assertThat(employee1.getDepartment().getName()).isEqualTo(department.getName());
         }
     }
 
@@ -55,25 +61,30 @@ public class EmployeeServiceTest {
     public void testGetAll() {
         Department department1 = new Department(Long.valueOf(1), "dept1", "department service unit test", "Paris", null);
         Department department2 = new Department(Long.valueOf(2), "dept2", "department service unit test", "London", null);
-        List<Department> departmentList = new ArrayList<>();
-        departmentList.add(department1);
-        departmentList.add(department2);
-        Page<Department> departmentPage = new PageImpl<>(departmentList);
-        when(departmentRepository.findAll(ArgumentMatchers.any(Pageable.class))).thenReturn(departmentPage);
+        Employee employee1 = new Employee(Long.valueOf(1), "Bart", "Vanden", Double.valueOf(2000), 35, "Architect", department1);
+        Employee employee2 = new Employee(Long.valueOf(2), "Bart", "Vanden", Double.valueOf(2000), 35, "Architect", department2);
+
+
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(employee1);
+        employeeList.add(employee2);
+        Page<Employee> employeePage = new PageImpl<>(employeeList);
+        when(employeeRepository.findAll(ArgumentMatchers.any(Pageable.class))).thenReturn(employeePage);
         Pageable pageable = PageRequest.of(1, 2);
-        Page<Department> departmentPage1 = departmentService.findAll(pageable);
-        Object[] depts = departmentPage1.stream().toArray();
-        Department dept1 = (Department) depts[0];
-        Department dept2 = (Department) depts[1];
-        assertThat(dept1.getName()).isEqualTo(department1.getName());
-        assertThat(dept1.getLocation()).isEqualTo(department1.getLocation());
-        assertThat(dept2.getName()).isEqualTo(department2.getName());
-        assertThat(dept2.getLocation()).isEqualTo(department2.getLocation());
+        Page<Employee> employeePage1 = employeeService.findAll(pageable);
+        Object[] empls = employeePage1.stream().toArray();
+        Employee empl1 = (Employee) empls[0];
+        Employee empl2 = (Employee) empls[1];
+        assertThat(empl1.getFirstName()).isEqualTo(empl1.getFirstName());
+        assertThat(empl1.getLastName()).isEqualTo(empl1.getLastName());
+        assertThat(empl2.getAge()).isEqualTo(empl2.getAge());
+        assertThat(empl2.getSalary()).isEqualTo(empl2.getSalary());
     }
 
     @Test
     public void testDelete() {
         Department department = new Department(Long.valueOf(1), "dept", "department service unit test", "Paris", null);
-        departmentService.delete(department);
+        Employee employee = new Employee(Long.valueOf(1), "Bart", "Vanden", Double.valueOf(2000), 35, "Architect", department);
+        employeeService.delete(employee);
     }
 }
