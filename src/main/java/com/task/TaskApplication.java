c5387da1a40f09b35e8d322b320093dff12536c1
jaber.abbas <jaber.h.abbas@gmail.com>
2023-02-20 17:40:28 +0100

    update

--- src/main/java/com/task/TaskApplication.java
+++ src/main/java/com/task/TaskApplication.java
@@ -9,9 +9,4 @@ public class TaskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApplication.class, args);
 	}
-
-
-	//Check TODOs on Entity API
-	//Services|Controller finish implementation of changes commented by Jaber
-	
 }
