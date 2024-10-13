ad465669c0abbab7b75170095a48b9f4a93ca6fa
Abbas <jaber.abbas@accenture.com>
2024-10-13 22:34:38 +0200

    junit empty class

---
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+
+
+class UserControllerTest {
+
+
+
+    @Test
+    void convertToDto() {
+
+    }
+
+    @Test
+    void convertToEntity() {
+    }
+}
